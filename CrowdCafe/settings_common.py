a15aa2b7c245e1439e6126495cfda08adccf35a2
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2014-03-27 17:51:52 +0100

    trying to fix the problem with Google Auth

--- CrowdCafe/settings_common.py
+++ CrowdCafe/settings_common.py
@@ -158,9 +158,7 @@ LOGGING = {
 AUTHENTICATION_BACKENDS = (
     'social_auth.backends.twitter.TwitterBackend',
     'social_auth.backends.facebook.FacebookBackend',
-    'social_auth.backends.google.GoogleOAuthBackend',
     'social_auth.backends.google.GoogleOAuth2Backend',
-    'social_auth.backends.google.GoogleBackend',
 #    'social_auth.backends.yahoo.YahooBackend',
 #    'social_auth.backends.browserid.BrowserIDBackend',
 #    'social_auth.backends.contrib.linkedin.LinkedinBackend',
