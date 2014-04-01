30ab78f643b6f65922320569e418fdb8c7bdf284
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2014-04-01 16:37:42 +0200

    django-mobi to route different views according to a device

--- CrowdCafe/settings_common.py
+++ CrowdCafe/settings_common.py
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social_auth',
+    'mobi',
     'storages',
     'landing',
     'account',
--- landing/views.py
+++ landing/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import get_object_or_404, render_to_response, redirect, Ht
 from django.http import HttpResponse
 from django.core.urlresolvers import reverse, reverse_lazy
 from django.template import RequestContext
-
-
+from mobi.decorators import detect_mobile
+@detect_mobile
 def Home(request):
 	if request.user.is_authenticated():
+		if request.mobile:
+			return redirect('cafe-home')
 		return redirect('kitchen-home')
 	else:
 		return redirect('welcome')
+@detect_mobile
 def Welcome(request):
-    return render_to_response('landing/welcome.html', context_instance=RequestContext(request))
+	if request.mobile:
+		return redirect('cafe-welcome')
+	return render_to_response('landing/welcome.html', context_instance=RequestContext(request))
     
