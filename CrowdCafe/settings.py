b057d06a4c6ed6d3850a6d4c662bfb72b64f43fb
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2014-07-29 12:06:27 +0200

    pipiline simplified

--- CrowdCafe/settings.py
+++ CrowdCafe/settings.py
@@ -484,9 +484,9 @@ SOCIAL_AUTH_PIPELINE = (
     'social_auth.backends.pipeline.user.get_username',
     'social_auth.backends.pipeline.user.create_user',
     'social_auth.backends.pipeline.social.associate_user',
-    'social_auth.backends.pipeline.user.update_user_details',
-    'social_auth.backends.pipeline.social.load_extra_data',
-    'account.pipes.get_user_addinfo',
+    #'social_auth.backends.pipeline.user.update_user_details',
+    #'social_auth.backends.pipeline.social.load_extra_data',
+    #'account.pipes.get_user_addinfo',
 )
 
 REST_FRAMEWORK = {
