e684355945dcce5834c763d381c0aebb62bef685
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2014-07-29 15:28:48 +0200

    render instructions

--- cafe/templatetags/customtags.py
+++ cafe/templatetags/customtags.py
@@ -10,4 +10,12 @@ def include_external(context,job_id):
 	t = Template(html)
 	return t.render(context)
 
+def render_instructions(context,job_id):
+	job = Job.objects.filter(pk = job_id).get()
+
+	html = job.description
+	t = Template(html)
+	return t.render(context)
+
 register.simple_tag(takes_context=True)(include_external)
+register.simple_tag(takes_context=True)(render_instructions)
